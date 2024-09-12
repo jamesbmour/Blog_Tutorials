67461ad8a3730daaa0d2972c634d3f05aaf51b44
James <jamesbrendamour7@yahoo.com>
2024-09-11 21:47:36 -0600

    Add various chart examples and layout configurations
    Enhanced the `p6_charts.py` with numerous chart examples including area, bar, line, scatter, Altair, Bokeh, Graphviz, Plotly, pydeck, and Vega charts. Updated `p7_layouts.py` with additional layout configurations, including sidebar, container, dialog, and tabs, to demonstrate different components and their usage.

--- Streamlit_Tutorials/Part_6_Charts/p6_charts.py
+++ Streamlit_Tutorials/Part_6_Charts/p6_charts.py
@@ -1,10 +1,15 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3), columns=["col1", "col2", "col3"]
-)
+# from streamlit_autorefresh import st_autorefresh
+
+# count = st_autorefresh(interval=4000, limit=200, key="refresh_counter")
+
+
+st.write("## Streamlit Part 6: Charts")
+st.write("### 1. Area Chart")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["col1", "col2", "col3"])
 
 st.area_chart(
     chart_data,
@@ -12,3 +17,232 @@ st.area_chart(
     y=["col2", "col3"],
     color=["#FF0000", "#0000FF"],  # Optional
 )
+
+st.write("### 2. Bar Chart")
+
+st.bar_chart(chart_data)
+
+st.write("### 3. Line Chart")
+# Line Chart
+st.line_chart(chart_data, x="col1", y="col2", color="col3")
+
+
+st.write("#### 3.1 Basic Line Chart")
+
+st.write("### 4. Map")
+df = pd.DataFrame(
+    {
+        "col1": np.random.randn(1000) / 50 + 37.76,
+        "col2": np.random.randn(1000) / 50 + -122.4,
+        "col3": np.random.randn(1000) * 100,
+        "col4": np.random.rand(1000, 4).tolist(),
+    }
+)
+
+st.map(df, latitude="col1", longitude="col2", size="col3", color="col4")
+
+
+st.write("### 5. Scatter Chat")
+scatter_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.scatter_chart(scatter_data)
+st.write("## Advanced Charts")
+st.write("### 6. Altair Chart")
+import altair as alt
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+    alt.Chart(chart_data)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
+
+
+st.write("### 7. Bokeh Chart")
+from bokeh.plotting import figure
+
+# import pandas as pd
+#
+# from bokeh.palettes import Spectral4
+# from bokeh.plotting import figure, show
+# from bokeh.sampledata.stocks import AAPL, GOOG, IBM, MSFT
+#
+# p = figure(width=800, height=250, x_axis_type="datetime")
+# p.title.text = "Click on legend entries to hide the corresponding lines"
+#
+# for data, name, color in zip(
+#     [AAPL, IBM, MSFT, GOOG], ["AAPL", "IBM", "MSFT", "GOOG"], Spectral4
+# ):
+#     df = pd.DataFrame(data)
+#     df["date"] = pd.to_datetime(df["date"])
+#     p.line(
+#         df["date"], df["close"], line_width=2, color=color, alpha=0.8, legend_label=name
+#     )
+#
+# p.legend.location = "top_left"
+# p.legend.click_policy = "hide"
+#
+# show(p)
+st.write("### 8. Graphviz Chart")
+import graphviz
+
+# Create a graphlib graph object
+graph = graphviz.Digraph()
+graph.edge("run", "intr")
+graph.edge("intr", "runbl")
+graph.edge("runbl", "run")
+graph.edge("run", "kernel")
+graph.edge("kernel", "zombie")
+graph.edge("kernel", "sleep")
+graph.edge("kernel", "runmem")
+graph.edge("sleep", "swap")
+graph.edge("swap", "runswap")
+graph.edge("runswap", "new")
+graph.edge("runswap", "runmem")
+graph.edge("new", "runmem")
+graph.edge("sleep", "runmem")
+
+st.graphviz_chart(graph)
+st.write("### 9. Plotly Chart")
+import plotly.figure_factory as ff
+import plotly.express as px
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ["Group 1", "Group 2", "Group 3"]
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
+df = px.data.gapminder()
+
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+import plotly.express as px
+
+st.subheader("Define a custom colorscale")
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="sepal_length",
+    color_continuous_scale="reds",
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+st.write("### 10. pydeck Chart")
+import pydeck as pdk
+
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+
+st.pydeck_chart(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=200,
+            ),
+        ],
+    )
+)
+st.write("### 11. pyplot Chart")
+import matplotlib.pyplot as plt
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+st.write("### 12. Vega Chart")
+from vega_datasets import data
+
+source = data.cars()
+
+chart = {
+    "mark": "point",
+    "encoding": {
+        "x": {
+            "field": "Horsepower",
+            "type": "quantitative",
+        },
+        "y": {
+            "field": "Miles_per_Gallon",
+            "type": "quantitative",
+        },
+        "color": {"field": "Origin", "type": "nominal"},
+        "shape": {"field": "Origin", "type": "nominal"},
+    },
+}
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Vega-Lite native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.vega_lite_chart(source, chart, theme="streamlit", use_container_width=True)
+with tab2:
+    st.vega_lite_chart(source, chart, theme=None, use_container_width=True)
+
+st.write("### 13. Vega-Lite Chart")
+
+st.write("### 14. Word Cloud")
--- Streamlit_Tutorials/Part_7_Layouts/p7_layouts.py
+++ Streamlit_Tutorials/Part_7_Layouts/p7_layouts.py
@@ -1,20 +1,17 @@
 import streamlit as st
 
-st.title("Streamlit Part 7: Layouts")
-# st.sidebar
-
-# using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+st.set_page_config(
+    page_title="Streamlit Part 7: Layouts",
+    page_icon="random",
+    layout="wide",
+    initial_sidebar_state="collapsed",
 )
 
-# using 'with' notation
-with st.sidebar:
-    st.write("This is a sidebar")
-    st.button("Press me")
+st.title("Streamlit Part 7: Layouts")
+
 
 # using st.columns
+st.write("### This is outside the columns")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -24,33 +21,85 @@ with col1:
 
 with col2:
     st.write("This is column 2")
-    st.button("Press me")
-
-
-# using st.tabs
-tabs = st.tabs()
-with tabs:
-    with st.tab("First"):
-        st.write("This is the first tab")
-    with st.tab("Second"):
-        st.write("This is the second tab")
-    with st.tab("Third"):
-        st.write("This is the third tab")
-
-# using st.expander
-expander = st.expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
-expander.write("...and even more details...")
-expander.write("...and even more details...")
-
-# using st.empty
-empty = st.empty()
-empty.text("This is some text")
-empty.markdown("This is some markdown")
-
+    st.button("Press me 2")
 
 # using st.container
+st.write("### This is outside the container")
 container = st.container()
-container.write("This is inside the container")
-container.write("This is also inside the container")
+with container:
+    st.write("This is inside the container")
+    st.button("Container 1")
 
+# using st.dialog
+st.write("### This is outside the dialog")
+
+
+# with st.dialog("This is a dialog"):
+#     st.write("This is inside the dialog")
+#     st.button("Container 2")
+@st.dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
+
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+
+# using st.empty
+st.write("### This is outside the empty")
+
+empty = st.empty()
+empty.write("This is inside the empty")
+empty.button("Empty")
+
+# using st.expander
+st.write("### This is outside the expander")
+with st.expander("This is an expander"):
+    st.write("This is inside the expander")
+    st.button("Expander")
+
+# using st.form
+st.write("### This is outside the form")
+with st.form("This is a form"):
+    st.write("This is inside the form")
+    name = st.text_input("Name")
+    st.form_submit_button("Form")
+    st.write(f"Name: {name}")
+
+
+# using st.popover
+st.write("### This is outside the popover")
+with st.popover("This is a popover"):
+    st.write("This is inside the popover")
+    st.button("Popover")
+
+# using st.sidebar
+st.write("### This is outside the sidebar")
+with st.sidebar:
+    st.write("This is inside the sidebar")
+    st.button("Sidebar")
+
+# using st.tabs
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.write("This is the cat tab")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with tab2:
+    st.write("This is the dog tab")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with tab3:
+    st.write("This is the owl tab")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
