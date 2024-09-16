8360a29aee4976336327fb4d2b9140ca9e205028
James <jamesbrendamour7@yahoo.com>
2024-09-16 03:57:47 -0600

    Add part 9 and 10 tutorials on Streamlit features
    Introduce tutorials detailing navigation pages and status elements. These additions include setting page configurations, titles, and specific Streamlit components for both navigation and status functionalities.

---
+++ Streamlit_Tutorials/Part_10_Nav_Pages/p10_Nav.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Part 10: Navigation Pages",
+    page_icon="random",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("Streamlit Part 10: Navigation and Pages")
+
+st.write("### st.navigation")
+
+st.write("### st.page")
+
+st.write("### st.set_page_link")
+
+st.write("### st.switch_page")
---
+++ Streamlit_Tutorials/Part_9_Status/p9_status.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Part 9: Status",
+    page_icon="random",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("Streamlit Part 9: Status Elements")
+
+st.write("### status.progress")
+
+st.write("### st.status")
+st.write("### st.spinner")
+st.write("### st.success")
+st.write("### st.error")
+st.write("### st.warning")
+st.write("### st.info")
+st.write("### st.exception")
+st.write("### st.balloons")
+st.write("### st.snow")
