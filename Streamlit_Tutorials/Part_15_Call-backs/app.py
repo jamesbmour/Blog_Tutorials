f6df03f29dcd83d961703cd7bce4d83f3670b103
James <jamesbrendamour7@yahoo.com>
2024-11-04 02:56:50 -0700

    Add new tutorial parts for custom components and callbacks
    Introduced new Python files for Streamlit tutorial parts 15 and 16, focusing on callbacks and custom components. This reorganization aims to improve the tutorial structure and provide clearer guidance on these advanced topics.

---
+++ Streamlit_Tutorials/Part_15_Call-backs/app.py
@@ -0,0 +1,6 @@
+# VIDEO_TITLE: Part 15: Call-backs
+# FILE_NAME: app.py
+import streamlit as st
+
+
+# SECTION 1:
---
+++ Streamlit_Tutorials/Part_16_Custom_Components/app.py
@@ -0,0 +1,5 @@
+# VIDEO_TITLE: Part 16: Custom Components
+# FILE_NAME: app.py
+import streamlit as st
+
+# SECTION 1:
